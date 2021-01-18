0c02f07354a65c3c0e18ff9e75955296fab88005
Harshita Mathur <hmathur0797@gmail.com>
2021-01-18 20:57:35 +0530

    Leet code-1480

---
+++ day1-1480.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    let sum = 0;
+    for(let i = 0; i < nums.length; i++){
+        sum = nums[i]+sum;
+        nums[i] = sum;
+    }
+    return nums;
+};
